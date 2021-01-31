26e7496d5db1e9fdff603a9f2004c2704ec0c233
Sam Aki <sam.k.aki8@gmail.com>
2021-01-31 17:11:26 +0000

    changed run script

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ app.use(express.json());
 
 // GET request at / - greeting!
 app.get('/', (req, res) => {
+  console.log('hi');
   const current = new Date();
   let mins = current.getMinutes();
+
   if (mins < 10) {
-    return (mins = `0${mins}`);
+    mins = `0${mins}`;
   }
   const greeting = `Welcome to the Leeds Feeds API. It's currently ${current.getHours()}:${mins} which means my newborn daughter, Willow, is either crying, screaming or crying. HOW DO I STOP IT?`;
   res.status(200).send(greeting);
